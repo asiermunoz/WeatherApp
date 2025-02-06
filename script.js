d18324503b88e780c0ef191b585d25140255dadc
asiermunoz <asiermunozlandi@gmail.com>
2025-02-06 10:53:24 -0400

    Weather App Finsihed (weathermap API implemented)

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ const currentDateTxt = document.querySelector('.current-date-txt')
 
 const apiKey = 'f2a34c51c330b0316678b998175cf66f';
 
+
+
 searchBtn.addEventListener('click', () => {
     if(cityInput.value.trim() != ''){
         updateWeatherInfo(cityInput.value);
@@ -33,7 +35,7 @@ cityInput.addEventListener('keydown', (event) =>{
 
 })
  async function getFetchData(endPoint, city){
-    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metrics`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
     const response = await fetch(apiUrl)
     return response.json()
 }
@@ -44,20 +46,18 @@ async function updateWeatherInfo(city){
     const weatherData = await getFetchData('weather', city)
     if(weatherData.cod != 200){
         showDisplaySection(notFoundSection)
-        console.log('holsaaaaaaa')
+
         return
     }
 
     console.log(weatherData)
-    console.log('holsa')
-
     const {
         name: country,
         main: {temp, humidity},
         weather: [{id, main}],
         wind: {speed}
 
-    } = weatherDate
+    } = weatherData
 
     countryTxt.textContent = country
     tempTxt.textContent = Math.round(temp) + ' ºC'
@@ -65,12 +65,25 @@ async function updateWeatherInfo(city){
     conditionTxt.textContent = main
     windValueTxt.textContent = speed + 'm/s'
 
+    weatherSummaryImg.src = `assets/weather/${getWeatherIcon(id)}`
+
 
 
 
     showDisplaySection(weatherInfoSection)
     
     
+}
+
+function getWeatherIcon(id){
+    if(id >= 200 && id <= 232) return 'thunderstorm.svg'
+    if (id >= 300 && id <= 321) return 'drizzle.svg'
+    if (id >= 500 && id <= 531) return 'rain.svg'
+    if (id >= 600 && id <= 622) return 'snow.svg'
+    if (id >= 701 && id <= 781) return 'atmosphere.svg'
+    if (id == 800) return 'clear.svg'
+    if (id >= 801 && id <= 804) return 'clouds.svg'
+    else return 'clouds.svg'
 }
 
 function showDisplaySection(section){
